95489e2e9e943f0423d0e546fe75b95788a20b9b
Emre Çınar <platanusdev@gmail.com>
2023-07-18 11:56:05 +0300

    Move Animasyon BlendTree Eklendi

---
+++ TPSPrototype/Assets/GameFolder/Scripts/Concreats/Animations/PlayerAnimation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TPSPrototype.Controllers;
+using UnityEngine;
+
+namespace TPSPrototype.Animations
+{
+    public class PlayerAnimation 
+    {
+        Animator _playerAnimator;
+        public PlayerAnimation(PlayerController playerController)
+        {
+                _playerAnimator = playerController.GetComponentInChildren<Animator>();
+        }
+
+        public void SetMoveAnimation(float moveSpeed)
+        {
+            _playerAnimator.SetFloat("moveSpeed",moveSpeed);
+        }
+    }
+}
+
--- TPSPrototype/Assets/GameFolder/Scripts/Concreats/Controllers/PlayerController.cs
+++ TPSPrototype/Assets/GameFolder/Scripts/Concreats/Controllers/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TPSPrototype.Abstracts.Inputs;
 using TPSPrototype.Abstracts.Movements;
+using TPSPrototype.Animations;
 using TPSPrototype.Movements;
 using UnityEngine;
 
@@ -13,19 +14,29 @@ namespace TPSPrototype.Controllers
     {
         IInput _input;
         IMover _mover;
-
+        PlayerAnimation _playerAnimation;
+        Vector3 _moveDirection;
         float _moveSpeed=5f;
         private void Awake()
         {
             _input = GetComponent<IInput>();
             _mover = new CharacterControllerMover(this, _moveSpeed);
+            _playerAnimation = new PlayerAnimation(this);
 
         }
 
         private void Update()
         {
-            Debug.Log(_input.MoveDirection);
-            _mover.MoveAction(_input.MoveDirection);
+            _moveDirection = _input.MoveDirection;
+            
+        }
+        private void FixedUpdate()
+        {
+            _mover.MoveAction(_moveDirection);
+        }
+        private void LateUpdate()
+        {
+            _playerAnimation.SetMoveAnimation(_moveDirection.magnitude);
         }
 
     }
